3d367c0dca172b8b207ebc9b8fabbd0069f8030e
Diana <kozachenkodiana03@mail.ru>
2022-01-18 19:17:35 +0700

    Created main components and added routing

--- todoshka/src/app/app.module.ts
+++ todoshka/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListsComponent } from './components/lists/lists.component';
+import { LabelsComponent } from './components/labels/labels.component';
+import { ArchiveComponent } from './components/archive/archive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListsComponent,
+    LabelsComponent,
+    ArchiveComponent
   ],
   imports: [
     BrowserModule,
