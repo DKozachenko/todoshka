e3fe588f89fdacb4199dc38e23b2728b15ee32d6
Diana <kozachenkodiana03@mail.ru>
2022-01-20 12:43:07 +0700

    Created service for lists

---
+++ todoshka/src/app/services/lists.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core'
+import {ListInterface} from "../interfaces/list.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListsService {
+  public lists: ListInterface[] = [
+    {
+      id: 1,
+      title: 'Кухня'
+    },
+    {
+      id: 2,
+      title: 'Дом'
+    },
+    {
+      id: 3,
+      title: 'Улица'
+    }
+  ]
+
+  constructor() { }
+
+  public delete(id: number): void {
+    this.lists = this.lists.filter((list: ListInterface) => {
+      return list.id !== id
+    })
+  }
+
+  public add(): void {
+    const len = this.lists.length + 1
+    this.lists.push({
+      id: len,
+      title: 'Random title'
+    })
+  }
+}
